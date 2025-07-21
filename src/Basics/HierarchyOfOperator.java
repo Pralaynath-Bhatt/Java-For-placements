6c551b3aac578e16064feded4ffcdc3977c3a5c5
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-07-21 14:30:33 +0530

    Conditional package init

---
+++ src/Basics/HierarchyOfOperator.java
@@ -0,0 +1,11 @@
+package Basics;
+
+import java.sql.SQLOutput;
+
+public class HierarchyOfOperator {
+    public static void main(String[] args) {
+        //LEFT TO RIGHT
+        //BO DM AS
+        System.out.println("CHAL GAYA");
+    }
+}
