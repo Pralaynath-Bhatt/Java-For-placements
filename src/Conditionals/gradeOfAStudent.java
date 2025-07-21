3d9099f4ebabd4ff55547315075545e84f3e376c
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-07-21 16:18:38 +0530

    Grade of a student

---
+++ src/Conditionals/gradeOfAStudent.java
@@ -0,0 +1,27 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class gradeOfAStudent {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your marks");
+            int marks= sc.nextInt();
+            if(marks>100){
+                System.out.println("Invalid Value");
+            }
+            else if(marks>90){
+                System.out.println("A");
+            }else if(marks>80){
+                System.out.println("B");
+            }else if(marks>70){
+                System.out.println("C");
+            }else if(marks>60){
+                System.out.println("D");
+            }else if(marks<60){
+                System.out.println("Grade is Not up to the mark");
+            }
+
+        }
+    }
