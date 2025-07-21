9de74e8a7f75a97ef9d948695c91d7233f85f75c
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-07-21 15:05:48 +0530

    Profit and loss

---
+++ src/Conditionals/ProfitAndLoss.java
@@ -0,0 +1,17 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class ProfitAndLoss {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Sell Price: ");
+        int SP = sc.nextInt();
+        System.out.println("Enter Cost Price: ");
+        int CP = sc.nextInt();
+        if (SP > CP) System.out.print("Profit of "+ (SP-CP) );
+        else if (CP > SP) System.out.print("Loss of "+ (CP-SP));
+        if (SP == CP) System.out.print("No Profit, No Loss");
+    }
+}
