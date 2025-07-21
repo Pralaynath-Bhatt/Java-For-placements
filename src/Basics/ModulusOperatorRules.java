5ba61afa88d6171db499f743470b18ae576ee3e1
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-07-21 13:29:36 +0530

    Sign of modulus

---
+++ src/Basics/ModulusOperatorRules.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class ModulusOperatorRules {
+    public static void main(String[] args) {
+        System.out.println(10%3);
+        System.out.println(10%-3);//sign of last variable does not matter= 1
+        System.out.println(-10%3);//sign of first variable matters -(10%3)= -1
+        System.out.println(-10%-3);//Sign of first variable matters so -(10%-3)= -1
+    }
+}
