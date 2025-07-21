9ebee540e0f39b318fb19f985ad9058c018675fb
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-07-21 13:21:42 +0530

    Square Of A Number

---
+++ src/Basics/SquareOfANumber.java
@@ -0,0 +1,15 @@
+package Basics;
+import java.util.Scanner;
+
+public class SquareOfANumber {
+    public static void main(String[] args) {
+        // write your code logic here
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println(a*a);
+
+    }
+}
+
+
