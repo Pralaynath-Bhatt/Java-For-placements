9e977f4e59700d752eb19d4146ff87c75602a4d6
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-09-03 01:14:14 +0530

    Selection Sort optimized

--- src/DSA/Sort/SelectionSort.java
+++ src/DSA/Sort/SelectionSort.java
@@ -1,20 +1,45 @@
 package DSA.Sort;
 
+import java.util.Scanner;
+
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr= {3,2,4,5,1};
+        int[] arr=giveArr();
         int n=arr.length;
-        for (int i = 0; i < n; i++) {
-            int smallest =i;
-            for (int j = i+1; j < n; j++) {
-                if(arr[j]<arr[smallest]){
+        System.out.println("unsorted");
+        print(arr);
+        for (int i = 0; i < n-1; i++) {
+            int smoll = Integer.MAX_VALUE;
+            int smallest=-1;
+            for (int j = i; j < n; j++) {
+                if(arr[j]<smoll){
+                    smoll=arr[j];
                     smallest= j;
                 }
                 }
+            if(smallest!=-1){
             int temp = arr[i];
             arr[i]=arr[smallest];
-            arr[smallest] = temp;
+            arr[smallest] = temp;}
+        }
+        System.out.println();
+        System.out.println("Sorted");
+        print(arr);
+    }
 
+    public static int[] giveArr() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of Array");
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]= sc.nextInt();
+        }
+        return arr;
+    }
+    private static void print(int[] arr) {
+        for(int elm: arr){
+            System.out.print(elm+" ");
         }
     }
 }
